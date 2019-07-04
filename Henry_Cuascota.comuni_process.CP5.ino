4d1799bf637103c535ba5c3bbd6c82b24959ffd7
Cuascota <44456154+Cuascota@users.noreply.github.com>
2019-07-03 22:42:20 -0500

    Add files via upload
    Arduino Cp5 Conexión

---
+++ Henry_Cuascota.comuni_process.CP5.ino
@@ -0,0 +1,18 @@
+#include <TimerOne.h>
+
+
+String dato;
+void setup() {
+  Serial.begin(9600);
+  Timer1.initialize(2000000);
+  Timer1.attachInterrupt(conteo);
+}
+
+void loop() {
+
+}
+
+void conteo(){
+  Serial.print(random(10,200));
+  
+  }
